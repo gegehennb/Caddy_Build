f3fef5de627d78e4cd0edf372fe2d69e990a3552
Cycle_Scence <chenyuchen1995@gmail.com>
2023-02-19 15:50:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,72 @@
-#!/usr/bin/env bash
-latest_version=$(curl -s "https://api.github.com/repos/caddyserver/caddy/releases/latest" | grep "tag_name" | cut -d\" -f4 | sed -e "s/^v//" -e "s/-.$//" | cut -d"v" -f 2)
-test -f caddy && current_version=$(./caddy_amd64 version | cut -d" " -f 1 | cut -d"v" -f 2) || current_version='caddy not found'
-if [[ ${latest_version} == ${current_version} ]]
-then
-    echo 'there is nothing to do'
-else
-    git config --local user.name 'GitHub Action'
-    git config --local user.email 'action@github.com'
-    go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
-    for arch in amd64 arm64
-    do
-        env GOOS=linux GOARCH=${arch} xcaddy build latest \
-        --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive \
-        --output caddy_${arch}
-        git add caddy_${arch}
-    done
-    git commit -am ${latest_version}
-    git push -v --progress
-fi
+name: Build caddy
+
+on:
+  workflow_dispatch:
+
+  release:
+    types: 
+      - 'prereleased' 
+      - 'published' 
+      - 'released'
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    strategy:
+      fail-fast: false
+      matrix:
+        config:
+        - {
+            name: "linux-amd64",
+            GOARCH: amd64,
+            GOOS: linux,
+            release: true
+          }
+        - {
+            name: "linux-arm64",
+            GOARCH: arm64,
+            GOOS: linux,
+            release: true
+          }
+
+    steps:
+    - name: Checkout
+      uses: actions/checkout@v3
+
+    - name: Install go
+      uses: actions/setup-go@v3
+      with:
+        go-version: '1.20'
+        check-latest: true
+    
+    - name: Install xcaddy
+      run: go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+    
+    - name: Build caddy
+      run: |
+        mkdir release-tmp
+        export GOARCH=${{ matrix.config.GOARCH }} 
+        export GOOS=${{ matrix.config.GOOS }} 
+        ~/go/bin/xcaddy build \
+          --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive \
+          --output ./release-tmp/caddy
+        
+    - name: Generate tar for Linux
+      run: |
+        mkdir release-ready
+        cd ./release-tmp
+        tar -zcvf ../release-ready/caddy-${{ matrix.config.name }}.tar.gz *
+
+    - name: Upload a Build Artifact
+      uses: actions/upload-artifact@v3
+      with:
+        name: caddy-${{ github.sha }}-${{ matrix.config.name }}
+        path: ./release-ready/*
+        
+    - name: Upload to GitHub Release for Linux
+      uses: svenstaro/upload-release-action@v2
+      with:
+        repo_token: ${{ secrets.ACCESS_TOKEN }}
+        file: ./release-ready/caddy-${{ matrix.config.name }}.tar.gz
+        tag: ${{ github.ref }}
+        overwrite: true
